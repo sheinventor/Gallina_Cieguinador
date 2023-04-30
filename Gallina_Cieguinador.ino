c14b2662d61b95f0b5790618f539e5a978c14a0a
SheInventor <110428835+sheinventor@users.noreply.github.com>
2023-04-29 20:37:45 -0600

    Add files via upload

---
+++ Gallina_Cieguinador.ino
@@ -0,0 +1,39 @@
+#define trigPin 2
+#define echoPin 3
+#define motorPin 5
+
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  // Envía un pulso de trigger
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Lee el tiempo de duración del pulso de echo
+  int duration = pulseIn(echoPin, HIGH);
+
+  // Calcula la distancia en cm
+  distance = duration / 58.2;
+
+  // Controla la velocidad del motor DC
+  if (distance > 20) {
+    // Si la distancia es mayor a 20 cm, el motor se detiene
+    digitalWrite(motorPin, LOW);
+  }
+  else {
+    // Si la distancia es menor a 20 cm, el motor se activa
+    analogWrite(motorPin, map(distance, 0, 20, 255, 0));
+  }
+
+  delay(50);
+}
+
